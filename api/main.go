19359e309ddeac1b6b4c142545c0923f5aac17b3
SyaifulDistroid <ahmadsyaifulislah@gmail.com>
2026-03-02 04:35:16 +0700

    Update Dummy

--- api/main.go
+++ api/main.go
@@ -25,35 +25,32 @@ const token = "supersecret"
 var app = fiber.New()
 
 func init() {
-	seed1 := Book{
+	seed := Book{
 		ID:     "e67d1777-99e9-4597-a33d-9cc2aa9ee44e",
 		Title:  "Dune",
 		Author: "Frank Herbert",
 		Year:   2000,
 	}
-	seed2 := Book{
-		ID:     "e67d1777-99e9-4597-a33d-9cc2aa9ee443",
-		Title:  "1984",
-		Author: "George Orwell",
-		Year:   1949,
-	}
-	seed3 := Book{
-		ID:     "e67d1777-99e9-4597-a33d-9cc2aa9ee445",
-		Title:  "Animal Farm",
-		Author: "George Orwell",
-		Year:   1945,
-	}
-	seed4 := Book{
-		ID:     "e67d1777-99e9-4597-a33d-9cc2aa9ee446",
-		Title:  "Brave New World",
-		Author: "Aldous Huxley",
-		Year:   1932,
-	}
+	books[seed.ID] = seed
 
-	books[seed1.ID] = seed1
+	seed2 := Book{
+		ID:     "e67d1777-99e9-4597-a33d-9cc2aa9ee44c",
+		Title:  "Dunex",
+		Author: "George Orwell",
+		Year:   2000,
+	}
+	books[seed2.ID] = seed2
+
+	seed3 := Book{
+		ID:     "e67d1777-99e9-4597-a33d-9cc2aa9ee44x",
+		Title:  "Dunez",
+		Author: "George Buss",
+		Year:   2000,
+	}
+	books[seed3.ID] = seed3
+
 	books[seed2.ID] = seed2
 	books[seed3.ID] = seed3
-	books[seed4.ID] = seed4
 
 	books["7"] = Book{"7", "The Hobbit", "J.R.R. Tolkien", 1937}
 	books["8"] = Book{"8", "Fahrenheit 451", "Ray Bradbury", 1953}
